0997aa891817a00eea022cba90a9083cffb3ac12
Leo Febba <leonardofebba@hotmail.com>
2021-09-24 18:48:13 -0300

    added smooth scroll and more style

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $('[data-group]').each(function(){
-	var $allTarget = $(this).find('[data-target]'),
+	const $allTarget = $(this).find('[data-target]'),
 			$allClick = $(this).find('[data-click]'),
 			activeClass = 'active';
 	
@@ -8,8 +8,7 @@ $('[data-group]').each(function(){
 	
 	$allClick.click(function(e){
 		e.preventDefault();
-		
-		var id = $(this).data('click'),
+		const id = $(this).data('click'),
 				$target = $('[data-target="' + id + '"]');
 		
 		$allClick.removeClass(activeClass);
@@ -17,5 +16,40 @@ $('[data-group]').each(function(){
 		
 		$target.addClass(activeClass);
 		$(this).addClass(activeClass);
+	});
+});
+
+$('.nav-menu a[href^="#"]').click(function(e){
+	e.preventDefault();
+	const id = $(this).attr('href'),
+	targetOffset = $(id).offset().top,
+	menuHeight = $('.menu').innerHeight(); 
+	
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
+
+$('.logo').click(function(e){
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: 0
+	}, 500);
+});
+
+$('section').each(function(){
+	const height = $(this).height(),
+	offsetTop = $(this).offset().top,
+	heightMenu = $('.menu').innerHeight(),
+	id = $(this).attr('id'),
+	$itemMenu = $('a[href^="#'+ id +'"]');
+
+	$(window).scroll(function(){
+		const topScroll = $(this).scrollTop();
+		if(offsetTop - heightMenu < topScroll && offsetTop + height - heightMenu > topScroll){
+			$itemMenu.addClass('active');
+		} else {
+			$itemMenu.removeClass('active');
+		}
 	});
 });
